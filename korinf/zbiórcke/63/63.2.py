330d86a8db40ab2130d621448a9d3b7e20ae9bf1
mitokac <mich120.game@gmail.com>
2023-03-14 12:12:41 +0100

    63.2 fin

--- korinf/zbiórcke/63/63.2.py
+++ korinf/zbiórcke/63/63.2.py
@@ -1,6 +1,19 @@
 liczby = []
+praw = True
+wyn = []
 
 with open('ciagi.txt') as plik:
     for line in plik:
         liczby.append(str(line.strip()))
 
+
+for i in range(len(liczby)):
+    praw = True
+    for j in range(1,len(liczby[i])):
+        if liczby[i][j] == liczby[i][j-1]:
+            praw = False
+
+    if praw == True:
+        wyn.append(liczby[i])
+
+print(len(wyn))
