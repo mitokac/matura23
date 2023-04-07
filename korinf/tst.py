b4cf314be4799d3ab4747bc9b4e07476d01146c2
mitokac <mich120.game@gmail.com>
2023-04-07 12:57:35 +0200

    18 fin

--- korinf/tst.py
+++ korinf/tst.py
@@ -1,14 +1,18 @@
 l = [1,2,6,23,67,335]
 p = 0
 k=len(l)-1
-x=6
+x=5
 sr=0
 
 while p<=k:
     sr = (k+p)//2
     if l[sr]==x:
-       print(l[sr])
+       print(sr)
+       break
     if l[sr]>x:
         k=sr-1
     if l[sr]<x:
         p=sr+1
+
+else:
+    print("brak")
